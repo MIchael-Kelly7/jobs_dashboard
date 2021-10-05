1b3abdbb8faebb2594b4c61c00046b2b240450df
Michael <44328815+MIchael-Kelly7@users.noreply.github.com>
2021-10-04 20:36:22 -0500

    Add files via upload
    Update to use chrome

--- tn_jobs.py
+++ tn_jobs.py
@@ -14,7 +14,13 @@ def error_exit():
     pass
 
 def scroll_page():
-    driver = webdriver.Firefox()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get('https://careers.edison.tn.gov/psc/hrprdrs/EMPLOYEE/HRMS/c/HRS_HRAM_FL.HRS_CG_SEARCH_FL.GBL?Page=HRS_APP_SCHJOB_FL&Action=U')
 
     time.sleep(1)
