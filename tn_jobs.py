ff5527be091d02b133296951397523f211798f10
Michael <44328815+MIchael-Kelly7@users.noreply.github.com>
2021-10-04 20:41:55 -0500

    Update tn_jobs.py
    Added OS import

--- tn_jobs.py
+++ tn_jobs.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
+import os
 
 def error_exit():
     pass
